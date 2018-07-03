7827a51447d0c2b75df00257fcd505eca05335c9
Vikas Kumar <negi.vikash925@gmail.com>
2018-07-03 23:21:03 +0530

    Updating existing items

--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -26,4 +26,8 @@ export class ShoppingListService{
 		this.ingredients.push(...ingredients);
 		this.ingredientsChanged.next(this.ingredients.slice());
 	}
+	updateIngredient(index:number, newIngredient:Ingredient){
+		this.ingredients[index]= newIngredient;
+		this.ingredientsChanged.next(this.ingredients.slice());
+	}
 }
