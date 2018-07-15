327ff1526316495a28c71c87d7c0a8d7580e57cd
Vikas Kumar <negi.vikash925@gmail.com>
2018-07-15 17:40:54 +0530

    Deleting ingredients and some finishing touch

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
+import { Subscription } from 'rxjs/Subscription';
 import { Recipe } from '../recipe.model';
 import { RecipeService } from '../recipe.service';
 
@@ -8,15 +9,15 @@ import { RecipeService } from '../recipe.service';
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
-export class RecipeListComponent implements OnInit {
-
+export class RecipeListComponent implements OnInit, OnDestroy {
+	subscription:Subscription;
 	recipes:Recipe[];
 	constructor(private recipeService:RecipeService,
 		private router:Router,
 		private route:ActivatedRoute) { }
 
 	ngOnInit() {
-		this.recipeService.recipeChanged
+		this.subscription= this.recipeService.recipeChanged
 		.subscribe(
 			(recipes:Recipe[])=>{
 				this.recipes = recipes;
@@ -26,5 +27,8 @@ export class RecipeListComponent implements OnInit {
 	}
 	onNewRecipe(){
 		this.router.navigate(['new'], {relativeTo:this.route });
+	}
+	ngOnDestroy(){
+		this.subscription.unsubscribe();
 	}
 }
