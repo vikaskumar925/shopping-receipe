15e478c8970eda966c76bfa9c0d25363dde5dafd
Vikas Kumar <negi.vikash925@gmail.com>
2018-07-17 23:18:50 +0530

    Sending PUT request to Save Data

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Response } from '@angular/http';
+import { DataStorageService } from '../shared/data-storage.service';
 
 @Component({
 	selector:'app-header',
@@ -6,5 +8,13 @@ import { Component } from '@angular/core';
 	styleUrls:['./header.component.css'],
 })
 export class HeaderComponent{
-	
+	constructor(private dataStorageService:DataStorageService){}
+	onSaveData(){
+		this.dataStorageService.storeRecipe()
+			.subscribe(
+				(response:Response)=>{
+					console.log(response);
+				},
+			);
+	}
 }
---
+++ src/app/shared/data-storage.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
+import { RecipeService } from '../recipes/recipe.service';
+
+@Injectable()
+
+export class DataStorageService {
+	constructor(private http:Http, private recipeService:RecipeService){}
+	storeRecipe(){
+		return this.http.put('https://shopping-recipe-b286b.firebaseio.com/recipes.json',this.recipeService.getRecipes());
+	}
+}
