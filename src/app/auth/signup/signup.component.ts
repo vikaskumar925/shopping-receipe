aaaa5cc6c60b66b69f9d4020a23456918e7d27de
Vikas Kumar <negi.vikash925@gmail.com>
2018-07-20 22:49:23 +0530

    Signing Users Up

--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -3,6 +3,7 @@ import {
     OnInit
 } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { AuthService } from '../auth.service';
 
 @Component({
     selector: 'app-signup',
@@ -11,13 +12,14 @@ import { NgForm } from '@angular/forms';
 })
 export class SignupComponent implements OnInit {
 
-    constructor() {}
+    constructor(private authService: AuthService) {}
 
     ngOnInit() {}
 
     onSignup(form:NgForm){
     	const email = form.value.email;
     	const password = form.value.password;
+    	this.authService.signupUser(email, password);
     }
 
 }
