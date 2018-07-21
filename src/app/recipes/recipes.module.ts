c4aea8a93446584c35d2d40c9c7acf5c39fbdeb7
Vikas Kumar <negi.vikash925@gmail.com>
2018-07-21 09:25:15 +0530

    Creating A recipe Feature Module

---
+++ src/app/recipes/recipes.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+
+import { RecipesComponent } from './recipes.component';
+import { RecipeListComponent } from './recipe-list/recipe-list.component';
+import { RecipeDetailComponent } from './recipe-detail/recipe-detail.component';
+import { RecipeItemComponent } from './recipe-list/recipe-item/recipe-item.component';
+import { RecipeStartComponent } from './recipe-start/recipe-start.component';
+import { RecipeEditComponent } from './recipe-edit/recipe-edit.component';
+import { DropdownDirective } from '../shared/dropdown.directive';
+
+@NgModule({
+	declarations:[
+		RecipesComponent,
+	    RecipeListComponent,
+	    RecipeDetailComponent,
+	    RecipeItemComponent,
+     	RecipeStartComponent,
+   		RecipeEditComponent,
+   		DropdownDirective,
+	],
+	imports:[
+		CommontModule,
+		ReactiveFormsModule,
+	],
+})
+export class RecipesModule { }
