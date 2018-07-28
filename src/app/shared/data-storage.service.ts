3867d1a3158c9a6986c661f459524bc995681ac8
Vikas Kumar <negi.vikash925@gmail.com>
2018-07-28 17:56:56 +0530

    Http Parameters

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { RecipeService } from '../recipes/recipe.service';
 import { Recipe } from '../recipes/recipe.model';
 import { AuthService } from '../auth/auth.service';
@@ -14,18 +14,20 @@ export class DataStorageService {
 	storeRecipe(){
 		const token = this.authService.getToken();
 		//const headers = new HttpHeaders().set('Authorization','Bearer sdfsd').append('contentType','json');
-		return this.httpClient.put('https://shopping-recipe-b286b.firebaseio.com/recipes.json?auth='+token,this.recipeService.getRecipes(),{
+		return this.httpClient.put('https://shopping-recipe-b286b.firebaseio.com/recipes.json',this.recipeService.getRecipes(),{
 			//observe:'events',
 			observe:'body',
+			params: new HttpParams().set('auth', token)
 			//headers: headers,
 		});
 	}
 	getRecipes(){
 		const token = this.authService.getToken();
 		//this.httpClient.get<Recipe[]>('https://shopping-recipe-b286b.firebaseio.com/recipes.json?auth='+token)
-		this.httpClient.get<Recipe[]>('https://shopping-recipe-b286b.firebaseio.com/recipes.json?auth='+token,{
+		this.httpClient.get<Recipe[]>('https://shopping-recipe-b286b.firebaseio.com/recipes.json',{
 			observe:'body',
 			responseType:'json',
+			params: new HttpParams().set('auth', token),
 		} )
 			.map(
 				(recipes) => {
