c1d97af087602f361c10a0e9beb598e99d80239f
Vikas Kumar <negi.vikash925@gmail.com>
2018-07-29 19:05:18 +0530

    Effects, AuthEffects and Actions

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { StoreModule } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
@@ -10,6 +11,7 @@ import { ShoppingListModule } from './shopping-list/shopping-list.module';
 import { AuthModule } from './auth/auth.module';
 import { CoreModule } from './core/core.module';
 import { reducers } from './store/app.reducers';
+import { AuthEffects } from './auth/store/auth.effects';
 
 @NgModule({
   declarations: [
@@ -24,6 +26,7 @@ import { reducers } from './store/app.reducers';
     AuthModule,
     CoreModule,
     StoreModule.forRoot(reducers),
+    EffectsModule.forRoot([AuthEffects])
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/auth/store/auth.effects.ts
@@ -0,0 +1,10 @@
+import { Actions, Effect } from '@ngrx/effects';
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class AuthEffects{
+	@Effect()
+	authSignup;
+
+	constructor(private actions$:Actions){}
+}
