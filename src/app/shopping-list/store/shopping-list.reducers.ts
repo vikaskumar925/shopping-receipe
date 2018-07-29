e2b95301bcf39a2fd883cd665ffd1c21aedca05b
Vikas Kumar <negi.vikash925@gmail.com>
2018-07-29 11:54:46 +0530

    Finishing Up first Reducer

--- src/app/shopping-list/store/shopping-list.reducers.ts
+++ src/app/shopping-list/store/shopping-list.reducers.ts
@@ -1,6 +1,5 @@
-import { Action } from '@ngrx/store';
+import * as ShoppingListActions from './shopping-list.actions';
 import { Ingredient } from '../../shared/ingredient.model';
-
 
 const initialState = {
 	Ingredients:[
@@ -8,14 +7,15 @@ const initialState = {
 		new Ingredient('Tomatoes',10),
 	]
 };
-export function shoppingListReducer(state = initialState, action:Action){
+export function shoppingListReducer(state = initialState, action:ShoppingListActions.ShoppingListActions){
 	switch(action.type){
-		case ADD_INGREDIENT: 
+		case ShoppingListActions.ADD_INGREDIENT: 
 			return {
 				...state,
-				ingredients:[...state.ingredients, action]
+				ingredients:[...state.ingredients, action.payload]
 
 			}
-	}
-	return state;
+		default:
+			return state;
+	}	
 }
